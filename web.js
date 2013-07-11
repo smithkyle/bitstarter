c0731ccee6db97fcd75e9b9052a816487e31866d
Kyle Smith <smith.kyle2@gmail.com>
2013-07-11 04:42:05 +0000

    first attempt at fs.readFileSync use

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var file = "index.html";
+
+buffer = fs.ReadFileSync(file);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
+  console.log(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
